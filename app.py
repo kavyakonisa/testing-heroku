2b556474382f14e1c351d99384eab642a504b51b
kavyakonisa <kavyakonisa1234@gmail.com>
2021-01-09 11:18:37 +0530

    Rest Apis

--- app.py
+++ app.py
@@ -42,19 +42,49 @@ def get_dish(dish_id):
 
   return jsonify(results)
 
+@app.route('/app/v1/resources/dishes', methods=['POST'])
+@auth.login_required
+def add_dish():
+  new_dish = {
+        'id': dishes[-1]['id'] + 1,
+        'title': request.json.get('title', ""),
+        'cost': request.json.get('cost', ""),
+         }
+  dishes.append(new_dish)
+  return jsonify({'new_dish_id': new_dish['id']}), 201
+  
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+
+
+@app.route('/app/v1/resources/dishes/<int:dish_id>', methods=['PUT'])
+@auth.login_required
+def update_dish(dish_id):
+    dish = [dish for dish in dishes if dish['id'] == dish_id]
+    if len(dish) == 0:
+        return make_response(jsonify({'error': 'No dish'}), 400)
+    if not request.json:
+        return make_response(jsonify({'error': 'Not in json'}), 404)
+    dish[0]['title'] = request.json.get('title', dish[0]['title'])
+    dish[0]['cost'] = request.json.get('cost', dish[0]['cost'])
+    return jsonify({'dish_id': dish[0]['id']})
+
 
 @app.route('/app/v1/resources/dishes/<int:dish_id>', methods=['DELETE'])
 def delete_task(dish_id):
   dish = [dish for dish in dishes if dish['id'] == dish_id]
   if len(dish) == 0:
-    abort(404)
+    return make_response(jsonify({'error': 'No dish'}), 400))
   dishes.remove(dish[0])
   return jsonify({'dish_id':dish[0]['id']})  
 
 @app.route('/app/v1/resources/dishes', methods=['DELETE'])
 def api_delete_all():
  if len(dishes) == 0:
-  abort(404)
+  return make_response(jsonify({'error': 'No dish'}), 400)
  while(len(dishes)!=0): 
   for dish in dishes:
    dishes.remove(dish) 
